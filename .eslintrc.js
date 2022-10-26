0f97424140618ee069a1c06ceb8cf1f4bf28a65e
devBuzz142 <devbuzz142@gmail.com>
2022-10-26 15:08:47 +0900

    config : eslint 추천 기본 plugin, extends 추가d

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,16 @@ module.exports = {
   // 린트에는 기본적으로 제공하는 rule 말고도 추가로 rule을 제공하는 다양한 plugin이 제공한다. 해당 plugin을 추가함으로써 plugin의 rule을 적용할 수 있다.
   // eslint-plugin- 으로 시작하는 패키지들
   // 플로그인만 추가한다고 관련 규칙이 활성화 되는 것이 아니다. extends나 rule에 추가해줘야 한다.
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import', '@typescript-eslint'],
   // 기본 rule들 설정해놓은 템플릿을 가져와서 쓰자!
   // eslint-config- 로 시작하는 패키지들
   extends: [
     'eslint:recommended', // eslint 기본 설정
     'plugin:react/recommended', // eslint 기본 설정
+    'plugin:react-hooks/recommended',
+    'plugini:jsx-a11y/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended', // eslint 기본 설정
     'plugin:storybook/recommended',
     'prettier', // eslitn의 style forammting에 있어서 prettier의 rlue로 우선하도록 eslint-config-prettier를 사용한다.
