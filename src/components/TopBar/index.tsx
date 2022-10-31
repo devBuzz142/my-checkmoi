58471e7ad6ae5f54f7a992d8f21dde9d26176ab1
devBuzz142 <devbuzz142@gmail.com>
2022-10-31 15:11:23 +0900

    feat: TopBar 초기 구현

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { FormEvent, useRef } from 'react';
 import * as S from './style';
 import { AppBar, Toolbar } from '../index';
 
 interface TopBarProps {}
 
 const TopBar = ({ ...props }: TopBarProps) => {
+  const serarchInputRef = useRef<null | HTMLInputElement>(null);
+
+  const handleSearchInputSumbit = (e: FormEvent<HTMLElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <AppBar color="primary" position="fixed">
-      <Toolbar>toolbar</Toolbar>
+    <AppBar color="primary" position="static">
+      <Toolbar>
+        <div>logo</div>
+        <S.SearchInputContainer>
+          <div>searchIcon</div>
+          <form onSubmit={handleSearchInputSumbit}>
+            <input type="text" ref={serarchInputRef} />
+          </form>
+        </S.SearchInputContainer>
+        <S.TempButtonContainer>
+          <button type="button">LogIn</button>
+        </S.TempButtonContainer>
+      </Toolbar>
     </AppBar>
   );
 };
--- src/components/TopBar/style.tsx
+++ src/components/TopBar/style.tsx
@@ -1,1 +1,19 @@
-export default {};
+import styled from '@emotion/styled';
+
+export const SearchInputContainer = styled.div`
+  background-color: rgba(255, 255, 255, 0.2);
+
+  border: 1px solid black;
+  border-radius: 5px;
+
+  font-size: 16px;
+
+  width: 256px;
+
+  margin-left: auto;
+  margin-right: auto;
+
+  display: flex;
+`;
+
+export const TempButtonContainer = styled.div``;
