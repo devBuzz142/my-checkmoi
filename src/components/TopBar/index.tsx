b16b72ee926a3fa7712cf407fe29d82d8948f9d1
devBuzz142 <devbuzz142@gmail.com>
2022-10-31 16:21:02 +0900

    feat: 로고 이미지, 타이틀 구현

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,11 +1,16 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { FormEvent, MouseEvent, useRef } from 'react';
 import * as S from './style';
 import { AppBar, Toolbar } from '../index';
+import LOGO_CHECKMOI from '../../assets/images/logo_checkmoi.png';
 
 interface TopBarProps {}
 
 const TopBar = ({ ...props }: TopBarProps) => {
   const serarchInputRef = useRef<null | HTMLInputElement>(null);
+
+  const handleLogoTitleClick = (e: MouseEvent<HTMLDivElement>) => {
+    //TODO - 라우팅 구현 예정
+  };
 
   const handleSearchInputSumbit = (e: FormEvent<HTMLElement>) => {
     e.preventDefault();
@@ -14,7 +19,17 @@ const TopBar = ({ ...props }: TopBarProps) => {
   return (
     <AppBar color="primary" position="static">
       <Toolbar>
-        <div>logo</div>
+        <S.LogoWrapper onClick={handleLogoTitleClick}>
+          <img
+            src={LOGO_CHECKMOI}
+            alt="logo_checkmoi.png"
+            width={40}
+            height={40}
+          />
+        </S.LogoWrapper>
+        <S.TitleWrapper onClick={handleLogoTitleClick}>
+          <S.Title>책모이</S.Title>
+        </S.TitleWrapper>
         <S.SearchInputContainer>
           <div>searchIcon</div>
           <form onSubmit={handleSearchInputSumbit}>
--- src/components/TopBar/style.tsx
+++ src/components/TopBar/style.tsx
@@ -1,5 +1,20 @@
 import styled from '@emotion/styled';
 
+export const LogoWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const TitleWrapper = styled.div`
+  margin-left: 4px;
+  cursor: pointer;
+`;
+
+export const Title = styled.h2`
+  font-family: monospace;
+  font-weight: 700;
+  letter-spacing: 0.3rem;
+`;
+
 export const SearchInputContainer = styled.div`
   background-color: rgba(255, 255, 255, 0.2);
 
