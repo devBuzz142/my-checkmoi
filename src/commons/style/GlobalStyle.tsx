0f54005ee403208cd00e85f54b0278084ba3d04a
devBuzz142 <devbuzz142@gmail.com>
2022-11-02 23:30:11 +0900

    feat: GlobalStyle init

---
+++ src/commons/style/GlobalStyle.tsx
@@ -0,0 +1,8 @@
+import { css, Global } from '@emotion/react';
+import React from 'react';
+
+const GlobalStyle = () => {
+  return <Global styles={css``} />;
+};
+
+export default GlobalStyle;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import theme from './commons/style/theme';
+import GlobalStyle from './commons/style/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
