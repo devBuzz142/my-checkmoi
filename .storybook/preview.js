dab05007906029bc822e0d7b0af65835dca3fcec
devBuzz142 <devbuzz142@gmail.com>
2022-11-03 17:12:43 +0900

    config: StoryBook preview 수정을 통한 context, globalStyle 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,22 @@
+import { ThemeProvider } from '@emotion/react';
+import theme from '../src/commons/style/theme';
+import GlobalStyle from '../src/commons/style/GlobalStyle';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story, context) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story {...context} />
+    </ThemeProvider>
+  ),
+];
