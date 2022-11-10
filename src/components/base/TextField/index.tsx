38187b9d9b01bfee205b22ccad741121903b9a6a
devBuzz142 <devbuzz142@gmail.com>
2022-11-10 21:17:42 +0900

    feat: props 추가

--- src/components/base/TextField/index.tsx
+++ src/components/base/TextField/index.tsx
@@ -1,13 +1,64 @@
-import React, { useEffect, useRef } from 'react';
+import React, {
+  ChangeEventHandler,
+  HTMLInputTypeAttribute,
+  useEffect,
+  useRef,
+} from 'react';
 import { InputBase } from '../../index';
 import * as S from './style';
 
 interface TextFieldProps {
+  autoComplete?: boolean;
+  autoFocus?: boolean;
+  color?: string;
+  defaultValue?: any;
+  disabled?: boolean;
+  disableUnderline?: boolean;
+  error?: boolean;
+  fullWidth?: boolean;
+  hiddenLabel?: boolean;
+  id?: string;
+  name?: string;
+  inputRef?: React.Ref<HTMLInputElement>;
+  margin?: 'dense' | 'none';
+  multiline?: boolean;
+  rows?: number;
+  maxRows?: number;
+  minRows?: number;
   label?: string;
   placeholder?: string;
+  readonly?: boolean;
+  required?: boolean;
+  type?: HTMLInputTypeAttribute;
+  value?: any;
+  // onChange?: ChangeEventHandler;
 }
 
-const TextField = ({ label = 'Standard', placeholder }: TextFieldProps) => {
+const TextField = ({
+  autoComplete = false,
+  autoFocus = false,
+  color = 'primary',
+  defaultValue,
+  disabled = false,
+  disableUnderline = false,
+  error = false,
+  fullWidth = false,
+  hiddenLabel = false,
+  id,
+  name,
+  inputRef,
+  margin,
+  multiline = false,
+  rows,
+  maxRows,
+  minRows,
+  label,
+  placeholder,
+  readonly = false,
+  required = false,
+  type,
+  value,
+}: TextFieldProps) => {
   return (
     <S.Container>
       {label && <S.Label>{label}</S.Label>}
