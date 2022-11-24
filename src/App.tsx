f2e3e6d2d27245be89213a37f4a6f84eb65df74e
devBuzz142 <devbuzz142@gmail.com>
2022-11-25 05:06:08 +0900

    feat: StudyPage 최초 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BookPage from './pages/BookPage';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
+import StudyPage from './pages/StudyPage';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/book" element={<BookPage />} />
+        <Route path="/study" element={<StudyPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/StudyPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PageTemplate from '../PageTemplate';
+
+interface StudyPageProps {}
+
+const StudyPage = ({ ...props }: StudyPageProps) => {
+  return (
+    <PageTemplate>
+      <div>studyPage</div>
+    </PageTemplate>
+  );
+};
+
+export default StudyPage;
