4b805f9852e2a099872df415e58cfe3b0038838e
devBuzz142 <devbuzz142@gmail.com>
2022-12-07 15:00:14 +0900

    feat: 썸네일 업로더 구현

--- src/pages/StudyOpenPage/index.tsx
+++ src/pages/StudyOpenPage/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import PageTemplate from '../PageTemplate';
 import * as S from './style';
 import { Button, TextField } from '../../components';
@@ -6,30 +6,92 @@ import { Button, TextField } from '../../components';
 interface StudyOpenPageProps {}
 
 const StudyOpenPage = ({ ...props }: StudyOpenPageProps) => {
+  const [imageUrl, setImageUrl] = useState<string>(
+    'https://via.placeholder.com/300.png',
+  );
+
+  const handleFileUploadChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (!e.target.files) return;
+    const file = e.target.files[0];
+
+    const MAX_FILE_SIZE = 1 * 1024 * 1024; // 1MB
+    if (file.size > MAX_FILE_SIZE) {
+      e.target.value = '';
+
+      console.error('업로드 할 수 있는 크기는 최대 1MB입니다.');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      setImageUrl(reader.result as string);
+    };
+  };
+
   return (
     <PageTemplate contentCenter>
       <S.Container>
         <S.UpperContainer>
           <S.LeftContainer>
-            <TextField label="책 제목" variant="filled" fullWidth />
-            <TextField label="스터디 이름" variant="standard" fullWidth />
-            <TextField label="스터디 인원" variant="standard" fullWidth />
+            <TextField label="책 제목" variant="filled" disabled fullWidth />
+            <TextField
+              label="스터디 이름"
+              variant="standard"
+              type={'text'}
+              fullWidth
+            />
+            <TextField
+              label="스터디 인원"
+              variant="standard"
+              type={'number'}
+              fullWidth
+            />
             <TextField
               label="스터디원 모집 시작"
               variant="standard"
+              type={'date'}
               fullWidth
             />
             <TextField
               label="스터디원 모집 마감"
               variant="standard"
+              type={'date'}
               fullWidth
             />
-            <TextField label="스터디 진행 시작" variant="standard" fullWidth />
-            <TextField label="스터디 진행 종료" variant="standard" fullWidth />
+            <TextField
+              label="스터디 진행 시작"
+              variant="standard"
+              type={'date'}
+              fullWidth
+            />
+            <TextField
+              label="스터디 진행 종료"
+              variant="standard"
+              type={'date'}
+              fullWidth
+            />
             <TextField label="스터디 모집 상태" variant="standard" fullWidth />
           </S.LeftContainer>
           <S.RightContainer>
-            <input type={'image'} alt="스터디 썸네일 입력" />
+            <form>
+              <S.ThumbnailTypo>스터디 썸네일</S.ThumbnailTypo>
+              <br />
+              <br />
+              <S.ThumbnailBoxLabel htmlFor="study-open-file-input">
+                <img src={imageUrl} alt="스터디 썸네일" />
+              </S.ThumbnailBoxLabel>
+              <input
+                id="study-open-file-input"
+                hidden
+                type={'file'}
+                accept="image/*"
+                alt="스터디 썸네일 입력"
+                onChange={handleFileUploadChange}
+              />
+            </form>
           </S.RightContainer>
         </S.UpperContainer>
         <S.UnderContainer>
--- src/pages/StudyOpenPage/style.tsx
+++ src/pages/StudyOpenPage/style.tsx
@@ -10,10 +10,51 @@ export const UpperContainer = styled.div`
 
 export const LeftContainer = styled.div`
   width: 512px;
+
+  padding: 8px;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
 `;
 
 export const RightContainer = styled.div`
   width: 512px;
+
+  padding: 8px;
+
+  form {
+    height: 100%;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const ThumbnailTypo = styled.div`
+  width: 100%;
+  height: 56px;
+
+  border-bottom: 1px solid ${(props) => props.theme.color.palette.grey[400]};
+
+  padding-top: 22px;
+
+  color: ${(props) => props.theme.color.palette.grey[400]};
+`;
+
+export const ThumbnailBoxLabel = styled.label`
+  height: 500px;
+  aspect-ratio: 2/3;
+
+  border: 1px solid ${(props) => props.theme.color.palette.grey[400]};
+  border-radius: 4px;
+
+  padding: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const UnderContainer = styled.div`
