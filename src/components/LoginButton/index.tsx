7816ba18e1925204f6440624405dc3da163cd282
devBuzz142 <devbuzz142@gmail.com>
2022-12-16 15:00:34 +0900

    feat: useModal 구현

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../base/Button';
+import Modal, { useModal } from '../base/Modal';
 
 interface LoginButtonProps {}
 
 const LoginButton = ({ ...props }: LoginButtonProps) => {
+  const { isModalOn, toggleModal, openModal, closeModal, modalRef } =
+    useModal();
+
   return (
-    <Button variant="contained" color="primary">
-      Login
-    </Button>
+    <>
+      <Button variant="contained" color="primary" onClick={toggleModal}>
+        Login
+      </Button>
+      <Modal isModalOn={isModalOn} modalRef={modalRef}>
+        카카오 로그인
+        <Button onClick={closeModal}>로그인</Button>
+      </Modal>
+    </>
   );
 };
 
--- src/components/base/Modal/index.tsx
+++ src/components/base/Modal/index.tsx
@@ -2,14 +2,38 @@ import React, {
   HtmlHTMLAttributes,
   ReactNode,
   RefObject,
+  useEffect,
   useRef,
   useState,
 } from 'react';
 import * as S from './style';
 
+export const useModal = (defaultOn = false) => {
+  const [isModalOn, setIsModalOn] = useState(defaultOn);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  const toggleModal = () => setIsModalOn(!isModalOn);
+  const openModal = () => setIsModalOn(true);
+  const closeModal = () => setIsModalOn(false);
+
+  useEffect(() => {
+    const handleBackgroundClick = () => {
+      if (!modalRef.current) return;
+
+      modalRef.current.addEventListener('click', closeModal);
+    };
+
+    handleBackgroundClick();
+    return handleBackgroundClick;
+  }, [modalRef.current]);
+
+  return { isModalOn, toggleModal, openModal, closeModal, modalRef };
+};
+
 interface ModalProps extends HtmlHTMLAttributes<HTMLDivElement> {
   children?: ReactNode;
   isModalOn?: boolean;
+  modalRef?: RefObject<HTMLDivElement>;
   center?: boolean;
   background?: boolean;
 }
@@ -18,6 +42,7 @@ const Modal = ({ ...props }: ModalProps) => {
   const {
     children,
     isModalOn = false,
+    modalRef,
     center = true,
     background = true,
   } = props;
@@ -28,6 +53,7 @@ const Modal = ({ ...props }: ModalProps) => {
       isModalOn={isModalOn}
       center={center}
       background={background}
+      ref={modalRef}
     >
       <S.ContentContainer className="modal-content">
         {children}
